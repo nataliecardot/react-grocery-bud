8c0a48681ae0edcb3818f5b015c7f31110cce3c6
Natalie-Cardot <nataliecardot@gmail.com>
2021-07-02 20:53:57 -0700

    Add static content

--- src/App.js
+++ src/App.js
@@ -10,7 +10,16 @@ function App() {
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
 
-  return <h2>grocery bud setup</h2>;
+  return (
+    <section className="section-center">
+      {/* alternative to onSubmit on form is onClick on a button */}
+      <form className="grocery-form"></form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">Clear Items</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
