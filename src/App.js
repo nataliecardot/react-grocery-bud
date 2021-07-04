594da3d340c0a4708020ac62c4340a93ff4cc2b4
Natalie-Cardot <nataliecardot@gmail.com>
2021-07-04 13:20:37 -0700

    Enable add item

--- src/App.js
+++ src/App.js
@@ -10,12 +10,43 @@ function App() {
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      // display alert if value is empty
+    } else if (name && isEditing) {
+      // deal with edit
+    } else {
+      // show alert
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName('');
+    }
+  };
+
   return (
     <section className="section-center">
       {/* alternative to onSubmit on form is onClick on a button */}
-      <form className="grocery-form"></form>
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Grocery Bud</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="e.g., eggs"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            autoFocus
+          />
+          {/* default type for button is submit in modern browsers */}
+          <button className="submit-btn">
+            {isEditing ? 'Edit' : 'Submit'}
+          </button>
+        </div>
+      </form>
       <div className="grocery-container">
-        <List />
+        <List items={list} />
         <button className="clear-btn">Clear Items</button>
       </div>
     </section>
